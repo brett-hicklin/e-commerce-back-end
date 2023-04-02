693b730c8183e0b6b19f13d3e0a4746b590de08b
Brett Hicklin <hicklin.brett@gmail.com>
2023-04-02 17:57:36 -0400

    Completed post and put requests for tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -42,12 +42,40 @@ router.get('/:id', async (req, res) => {
 
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+
+  try { 
+    const newTag = await Tag.create(req.body)
+    res.status(200).json(newTag)
+  
+  } catch (err) { 
+    res.status(400).json(err)
+    
+  }  
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try { 
+    const updatedTag = await Tag.update(
+    {
+      // All the fields you can update and the data attached to the request body.
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    },
+  
+  )
+
+  res.status(200).json(updatedTag)
+    
+} catch (err){
+  res.status(500).json(err)
+}
 });
 
 router.delete('/:id', async (req, res) => {
