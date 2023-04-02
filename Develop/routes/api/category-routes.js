ab0790c48b927fb858ba8780fca99119e064532d
Brett Hicklin <hicklin.brett@gmail.com>
2023-04-02 17:53:58 -0400

    Completed post and put requests for categories

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -40,12 +40,42 @@ router.get('/:id', async (req, res) => {
 
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  
+try { 
+  const newCategory = await Category.create(req.body)
+  res.status(200).json(newCategory)
+
+} catch (err) { 
+  res.status(400).json(err)
+  
+}
+
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try { 
+    const updatedCategory = await Category.update(
+    {
+      // All the fields you can update and the data attached to the request body.
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    },
+  
+  )
+
+  res.status(200).json(updatedCategory)
+    
+} catch (err){
+  res.status(500).json(err)
+}
 });
 
 router.delete('/:id', async (req, res) => {
